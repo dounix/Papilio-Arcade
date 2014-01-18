b2229f10a47e20cf3484fe2270fa10d9cf9a06df
dounix <jims@ape.camptaylor.net>
2014-01-18 01:13:19 -0500

    Added checksums for US ROMS

--- pacman_rel004_sp3e_papilio/scripts/build_roms_pacman_us.bat
+++ pacman_rel004_sp3e_papilio/scripts/build_roms_pacman_us.bat
@@ -1,16 +1,24 @@
 @echo off
 
 REM SHA1 sums of files required
+REM SHA1 sums of files required This script is for the US Version
 REM 8d0268dee78e47c712202b0ec4f1f51109b1f2a5 *82s123.7f
 REM bbcec0570aeceb582ff8238a4bc8546a23430081 *82s126.1m
 REM 0c4d0bee858b97632411c440bea6948a74759746 *82s126.3m
 REM 19097b5f60d1030f8b82d9f1d3a241f93e5c75d6 *82s126.4a
+REM Puckman - Japan not needed
 REM 87117ba5082cd7a615b4ec7c02dd819003fbd669 *namcopac.6e
 REM 326dbbf94c6fa2e96613dedb53702f8832b47d59 *namcopac.6f
 REM 7e1945f6eb51f2e51806d0439f975f7a2889b9b8 *namcopac.6h
 REM 01b4c38108d9dc4e48da4f8d685248e1e6821377 *namcopac.6j
+REM Used by both
 REM 06ef227747a440831c9a3a613b76693d52a2f0a9 *pacman.5e
 REM 4a937ac02216ea8c96477d4a15522070507fb599 *pacman.5f
+REM Pac-Man - US
+REM e87e059c5be45753f7e9f33dff851f16d6751181 *pacman.6e
+REM 674d3a7f00d8be5e38b1fdc208ebef5a92d38329 *pacman.6f
+REM 8e47e8c2c4d6117d174cdac150392042d3e0a881 *pacman.6h
+REM d4a70d56bb01d27d094d73db8667ffb00ca69cb9 *pacman.6j
 
 set rom_path_src=..\roms\pacman
 set rom_path=..\build
